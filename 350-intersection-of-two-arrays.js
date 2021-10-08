d46586f5eb4dc7221a7b7f5560ee68778e5c2472
Serhii T <tocarchuk@gmail.com>
2021-10-08 11:54:35 +0300

    Hash Map pattern: #350

---
+++ 350-intersection-of-two-arrays.js
@@ -0,0 +1,35 @@
+// 350. Intersection of Two Arrays II
+// Given two integer arrays nums1 and nums2, return an array of their intersection. 
+// Each element in the result must appear as many times as it shows in both arrays 
+// and you may return the result in any order.
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+
+function intersect(arr1, arr2){
+  // If arr1 is larger than arr2, swap the arrays
+  if( arr1.length > arr2.length ) {
+    let temp = arr1;
+    arr1 = arr2;
+    arr2 = temp;
+  }
+  
+  let result = [];
+  // each element in nums1 add it to the hash map
+  let map = new Map();
+  for( let i = 0; i < arr1.length; i++ ){
+    map.set(arr1[i], map.get(arr1[i]) + 1 || 1);
+  }
+  for( let j = 0; j < arr2.length; j++ ){
+    if( map.has(arr2[j]) ){
+      result.push(arr2[j]);
+      // Decrement the count in the hash map
+      map.set(arr2[j], map.get(arr2[j]) - 1)
+      if( map.get(arr2[j]) === 0 ) map.delete(arr2[j])
+    }
+  }
+  return result;
+}
+
+console.log(intersect([1,2,2,1,4,5,7], [2,2,7]))
