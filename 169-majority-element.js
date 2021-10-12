09299619d52bf1ab22afc1bcfca52c5d35a66490
Serhii T <tocarchuk@gmail.com>
2021-10-12 22:24:07 +0300

    Algirithms Object: #136

--- 169-majority-element.js
+++ 169-majority-element.js
@@ -4,11 +4,16 @@
 // You may assume that the majority element always exists in the array.
 
 function majorityElement(nums){
-  const map = new Map();
-  for( let i = 0; i < nums.length; i++ ){
-    map.set(nums[i], map.get(nums[i]) + 1 || 1)
-    if( map.get(nums[i]) > nums.length / 2) return nums[i];
-  }
+// Hash Map
+  // const map = new Map();
+  // for( let i = 0; i < nums.length; i++ ){
+  //   map.set(nums[i], map.get(nums[i]) + 1 || 1)
+  //   if( map.get(nums[i]) > nums.length / 2) return nums[i];
+  // }
+
+// Object
+const data = {};
+
 }
 
 console.log(majorityElement([2,2,1,1,1,2,2,1,1,1,1]))
