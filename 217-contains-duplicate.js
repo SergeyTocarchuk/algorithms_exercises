f10d50f2099d2da463692c0bf6dda1bc4efa79c7
Serhii T <tocarchuk@gmail.com>
2021-10-13 11:37:29 +0300

    Algorithms Object: #217

--- 217-contains-duplicate.js
+++ 217-contains-duplicate.js
@@ -3,15 +3,30 @@
 // and return false if every element is distinct.
 
 function containsDuplicate(nums) {
-  const map = new Map();
-  for( let i = 0; i < nums.length; i++ ){
-    if( map.has(nums[i]) ){
-      return true;
-    } else {
-      map.set(nums[i], 1);
+  //   const map = new Map();
+  //   for( let i = 0; i < nums.length; i++ ){
+  //     if( map.has(nums[i]) ){
+  //       return true;
+  //     } else {
+  //       map.set(nums[i], 1);
+  //     }
+  //   }
+  
+    const object = {};
+    for( let i = 0; i < nums.length; i++ ){
+      const key = nums[i];
+      if( object[key] ){
+        delete object[key];
+      } else {
+        object[key] = true;
+      }
     }
+    for( let key in object ){
+      if( object[key] ) {
+        return false;
+      }
+    }
+    return true;
   }
-  return false;
-}
-
-console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+  
+  console.log(containsDuplicate([1,3,3,2,2,1]))
