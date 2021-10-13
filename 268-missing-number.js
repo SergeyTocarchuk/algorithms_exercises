b76e6f09619f5a76d8f864efffe86c3eea72946a
Serhii T <tocarchuk@gmail.com>
2021-10-13 11:19:45 +0300

    Algorithms Object: #268

--- 268-missing-number.js
+++ 268-missing-number.js
@@ -2,15 +2,25 @@
 // return the only number in the range that is missing from the array.
 
 function missingNumber(nums){
-  const map = new Map();
-  for( let i = 0; i < nums.length; i++ ){
-    map.set(nums[i], 1);
-  }
-  for( let j = 0; j < nums.length + 1; j++ ){
-    if( !map.has(j) ){
-      return j;
+  //   const map = new Map();
+  //   for( let i = 0; i < nums.length; i++ ){
+  //     map.set(nums[i], 1);
+  //   }
+  //   for( let j = 0; j < nums.length + 1; j++ ){
+  //     if( !map.has(j) ){
+  //       return j;
+  //     }
+  //   }
+  
+    const object = {};
+    for (let i = 0; i < nums.length; i++){
+      object[nums[i]] = true;
+    }
+    for( let j = 0; j < nums.length; j++ ){
+      if( !(object[j]) ){
+        return j;
+      }
     }
   }
-}
-
-console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
+  
+  console.log(missingNumber([8,9,6,4,2,3,5,7,0,1,11]))
