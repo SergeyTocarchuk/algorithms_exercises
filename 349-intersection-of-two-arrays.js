7cc9e2fd173fab3c84cc18e9b8dec765b10bfc10
Serhii T <tocarchuk@gmail.com>
2021-10-14 14:58:03 +0300

    Algorithms Object: #349

--- 349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays.js
@@ -3,21 +3,45 @@
 // Each element in the result must be unique and you may return the result in any order.
 
 function intersect(arr1, arr2){
-  const map = new Map();
-  let result = [];
-
-  for( let i = 0; i < arr1.length; i++ ){
-    if( !map.has(arr1[i]) ){
-      map.set(arr1[i], 1)
+  //   const map = new Map();
+  //   let result = [];
+  
+  //   for( let i = 0; i < arr1.length; i++ ){
+  //     if( !map.has(arr1[i]) ){
+  //       map.set(arr1[i], 1)
+  //     }
+  //   }
+  //   for( let j = 0; j < arr2.length; j++ ){
+  //     if( map.has(arr2[j]) ){
+  //       result.push(arr2[j]);
+  //       map.delete(arr2[j]);
+  //     }
+  //   }
+  //   return result;
+  
+    const object = {}
+    let result = []
+    // If nums1 is larger than nums2, swap the arrays:
+    if( arr1.length < arr2.length ){
+      let temp = arr1;
+      arr1 = arr2;
+      arr2 = temp;
     }
-  }
-  for( let j = 0; j < arr2.length; j++ ){
-    if( map.has(arr2[j]) ){
-      result.push(arr2[j]);
-      map.delete(arr2[j]);
+    
+    for( let i = 0; i < arr1.length; i++ ){
+      const key = arr1[i];
+      if( !(object[key]) ){
+        object[key] = true;
+      }
     }
+    for( let j = 0; j < arr2.length; j++ ){
+      const prop = arr2[j];
+      if( object[prop] ){
+        result.push(prop);
+        delete object[prop];
+      }
+    }
+    return result;
   }
-  return result;
-}
-
-console.log(intersect([1,2,2,1,4,5,7], [2,2,7]))
+  
+  console.log(intersect([1,2,2,1,4,5,7], [2,2,7]));
