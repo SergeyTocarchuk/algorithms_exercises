9489265a7a05e9db532df39afec45beb20e4eb9e
Serhii T <tocarchuk@gmail.com>
2021-10-18 12:30:32 +0300

    Algorithms Object: 387

--- 387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string.js
@@ -2,16 +2,31 @@
 // If it does not exist, return -1.
 
 function firstUniqChar(str){
-  const map = new Map();
-  for( let i = 0; i < str.length; i++ ){
-    map.set(str[i], map.get(str[i])+1 || 1)
-  }
-  for( let i = 0; i < str.length; i++ ){
-    if( map.get(str[i]) === 1 ){
-      return i;
+  //   const map = new Map();
+  //   for( let i = 0; i < str.length; i++ ){
+  //     map.set(str[i], map.get(str[i])+1 || 1)
+  //   }
+  //   for( let i = 0; i < str.length; i++ ){
+  //     if( map.get(str[i]) === 1 ){
+  //       return i;
+  //     }
+  //   }
+  //   return -1;
+  
+    const object = {};
+    for( let i = 0; i < str.length; i++ ){
+      const key = str[i];
+      if( object[key] ){
+        object[key]++;
+      } else {
+        object[key] = 1;
+      }
     }
+    for( let j = 0; j < str.length; j++ ){
+      const key = str[j];
+      if( object[key] === 1 ) return j;
+    }
+    return -1;
   }
-  return -1;
-}
-
-console.log(firstUniqChar('propper'));
+  
+  console.log(firstUniqChar('propper'));
