cf334b9a81dee83a99fbe97f6e43e12b8e798421
Serhii T <tocarchuk@gmail.com>
2021-10-22 07:11:36 +0300

    Algorithms Object and String: 242

--- 242-valid-anagram.js
+++ 242-valid-anagram.js
@@ -2,19 +2,32 @@
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 function isAnagram(str1, str2){
-  if( str1.length !== str2.length ) return false;
-  const map = new Map();
-  for( let i = 0; i < str1.length; i++ ){
-    map.set(str1[i], map.get(str1[i]) + 1 || 1)
+  //   if( str1.length !== str2.length ) return false;
+  //   const map = new Map();
+  //   for( let i = 0; i < str1.length; i++ ){
+  //     map.set(str1[i], map.get(str1[i]) + 1 || 1)
+  //   }
+  //   for( let j = 0; j < str2.length; j++ ){
+  //     if( map.has(str2[j]) ){
+  //       map.set( str2[j], map.get(str2[j]) - 1 )
+  //       if( map.get(str2[j]) === 0 ) map.delete(str2[j])
+  //     } else return false;
+  //   }
+  //   if( map.size === 0 ) return true;
+  //   else return false;
+    
+    if( str1.length !== str2.length ) return false;
+    const obj = {}
+    for( let i = 0; i < str1.length; i++ ){
+      const key1 = str1[i];
+      const key2 = str2[i];
+      obj[key1] ? obj[key1]++ : obj[key1] = 1;
+      if( obj[key1] === 0 ) delete obj[key1];
+      obj[key2] ? obj[key2]-- : obj[key2] = -1;
+      if( obj[key2] === 0 ) delete obj[key2]; 
+    }
+    if( Object.keys(obj).length === 0 ) return true;
+    else return false; 
   }
-  for( let j = 0; j < str2.length; j++ ){
-    if( map.has(str2[j]) ){
-      map.set( str2[j], map.get(str2[j]) - 1 )
-      if( map.get(str2[j]) === 0 ) map.delete(str2[j])
-    } else return false;
-  }
-  if( map.size === 0 ) return true;
-  else return false;
-}
-
-console.log(isAnagram('racecar', 'racecal'))
+  
+  console.log(isAnagram('racecar', 'racecal'))
